a4730e6ad3f45284d957d85cac865013c8034841
Tim Fuller <tjfulle@sandia.gov>
2019-05-05 20:04:04 -0600

    environ to own module

--- lib/pymod/pymod/environ/_environ.py
+++ lib/pymod/pymod/environ/_environ.py
@@ -2,7 +2,6 @@ import os
 from argparse import Namespace
 
 import pymod.names
-from contrib.util.lang import Singleton
 from contrib.util.misc import str2dict, dict2str, boolean, split, join, pop
 from contrib.ordereddict_backport import OrderedDict
 
@@ -108,48 +107,3 @@ class Environ(OrderedDict):
         if count > 0:
             current_path.meta[value] = (count, priority)
         self.set_path(current_path)
-
-
-environ = Singleton(Environ)
-
-def reset():
-    global environ
-    environ = Environ()
-
-
-def get(key, default=None):
-    return environ.get(key, default)
-
-def get_bool(key):
-    return eniron.get_bool(key)
-
-def set(key, value):
-    return environ.set(key, value)
-
-def unset(key):
-    return environ.unset(key)
-
-def set_alias(key, value):
-    return environ.set_alias(key, value)
-
-def unset_alias(key):
-    return environ.unset_alias(key)
-
-def set_shell_function(key, value):
-    return environ.set_shell_function(key, value)
-
-def unset_shell_function(key):
-    return environ.unset_shell_function(key)
-
-def append_path(key, value, sep=os.pathsep):
-    return environ.append_path(key, value, sep)
-
-def prepend_path(key, value, sep=os.pathsep):
-    return environ.prepend_path(key, value, sep)
-
-def remove_path(key, value, sep=os.pathsep):
-    return environ.remove_path(key, value, sep)
-
-def get_path(key, sep=os.pathsep):
-    path = environ.get_path(key, sep=sep)
-    return path.value
