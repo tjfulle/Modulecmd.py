80430f2a45ba80a312e3e3a814261c703e4f346a
Tim Fuller <tjfulle@sandia.gov>
2019-05-07 11:55:56 -0600

    adding swap test

--- lib/pymod/pymod/mc/unload.py
+++ lib/pymod/pymod/mc/unload.py
@@ -8,7 +8,6 @@ def unload(name, tolerant=False):
     """Unload the module given by `name`"""
     module = pymod.modulepath.get(name)
     if module is not None and module.is_loaded:
-        mode = UNLOAD
         pymod.mc.execmodule(module, pymod.modes.unload)
         return None
 
