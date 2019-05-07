ad79bf630220069e88e376de8fefc2fda48d7a94
Tim Fuller <tjfulle@sandia.gov>
2019-05-07 13:06:13 -0600

    version to 3.0

--- lib/pymod/pymod/__init__.py
+++ lib/pymod/pymod/__init__.py
@@ -1,5 +1,5 @@
-pymod_major_version = 2
+pymod_major_version = 3
 pymod_minor_version = 0
-pymod_micro_version = 1
+pymod_micro_version = 0
 pymod_version = '{}.{}.{}'.format(
     pymod_major_version, pymod_minor_version, pymod_micro_version)
