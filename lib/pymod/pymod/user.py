87bf8e03621f9d0376d54c42ea39e3e8fc60122f
Tim Fuller <tjfulle@sandia.gov>
2019-05-09 10:21:49 -0600

    load correct user env file

--- lib/pymod/pymod/user.py
+++ lib/pymod/pymod/user.py
@@ -7,7 +7,10 @@ from llnl.util.lang import load_module_from_file, Singleton
 class UserEnv:
     def __init__(self, filename):
         # Load user module, if found
-        self._module = load_module_from_file('pymod._user', filename)
+        if filename is None:
+            self._module = None
+        else:
+            self._module = load_module_from_file('pymod._user', filename)
 
     def __getattr__(self, attr):
         if self._module is None:
@@ -17,16 +20,17 @@ class UserEnv:
 
 def _user_env():
     name = pymod.config.get('user_env_filename')
-    if os.path.exists(name):
+    op = os.path
+    if op.exists(name) and op.realpath(name) != op.realpath(__file__):
         filename = name
-    elif os.path.exists(os.path.join(pymod.paths.user_config_path, name)):
-        filename = os.path.join(pymod.paths.user_config_path, name)
-    elif os.path.exists(os.path.join(pymod.paths.user_config_path, 'user.py')):
-        filename = os.path.join(pymod.paths.user_config_path, 'user.py')
+    elif op.exists(op.join(pymod.paths.user_config_path, name)):
+        filename = op.join(pymod.paths.user_config_path, name)
+    elif op.exists(op.join(pymod.paths.user_config_path, 'user.py')):
+        filename = op.join(pymod.paths.user_config_path, 'user.py')
     else:
         filename = None
     if filename is not None:
-        assert os.path.isfile(os.path.realpath(filename))
+        assert op.isfile(op.realpath(filename))
     return UserEnv(filename)
 
 
