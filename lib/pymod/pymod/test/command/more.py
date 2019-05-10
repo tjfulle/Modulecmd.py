ff1d94a0e0e09506e48ef830414099ff66df8af6
Tim Fuller <tjfulle@sandia.gov>
2019-05-10 06:26:54 -0600

    adding more test

---
+++ lib/pymod/pymod/test/command/more.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+
+import pymod.mc
+import pymod.environ
+from pymod.main import PymodCommand
+
+
+@pytest.fixture()
+def modules_path(tmpdir, namespace, modulecmds):
+    m = modulecmds
+    one = tmpdir.mkdir('1')
+    one.join('a.py').write(m.setenv('a'))
+    ns = namespace()
+    ns.one = one.strpath
+    return ns
+
+
+def test_more(modules_path, mock_modulepath):
+    more = PymodCommand('more')
+    mp = mock_modulepath(modules_path.one)
+    more('a')
