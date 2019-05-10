abb1615409cc57626f3de955fbee86dc1fe0dfe2
Tim Fuller <tjfulle@sandia.gov>
2019-05-10 06:25:31 -0600

    cleaning  up

--- lib/pymod/pymod/command/common/get_entity_text.py
+++ lib/pymod/pymod/command/common/get_entity_text.py
@@ -1,14 +1,12 @@
 import pymod.paths
 import pymod.modulepath
+import pymod.collection
 
 
 def get_entity_text(name):
     module = pymod.modulepath.get(name)
     if module is not None:
         return open(module.filename).read()
-    elif name in pymod.collections:
-        collection = pymod.collections.get(name)
-        return json.dumps(collection, default=serialize, indent=2)
-    elif os.path.isfile(os.path.join(pymod.paths.dot_dir, name + '.json')):
-        return open(os.path.join(cfg.dot_dir, name + '.json')).read()
+    elif pymod.collection.contains(name):
+        return str(pymod.collection.get(name))
     raise Exception('Unknown named entity {0!r}'.format(name))
