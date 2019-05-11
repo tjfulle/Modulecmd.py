ffaf0ca63bd84c2cc2057c4aa81f13e00199f4ea
Tim Fuller <tjfulle@sandia.gov>
2019-05-11 08:09:16 -0600

    cat codecov

--- lib/pymod/pymod/test/command/cat.py
+++ lib/pymod/pymod/test/command/cat.py
@@ -20,3 +20,6 @@ def test_command_cat(modules_path, mock_modulepath):
     cat = PymodCommand('cat')
     mp = mock_modulepath(modules_path.one)
     cat('a')
+
+    with pytest.raises(Exception):
+        cat('fake')
