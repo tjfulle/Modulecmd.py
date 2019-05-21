df2ddd90d0fb4d2729f9aefaea31f6c317ee19aa
Tim Fuller <tjfulle@sandia.gov>
2019-05-21 09:02:16 -0600

    adding missing files

---
+++ lib/pymod/pymod/command/remove.py
@@ -0,0 +1,16 @@
+import pymod.mc
+
+description = 'Remove saved collection of modules'
+level = 'short'
+section = 'collections'
+
+
+def setup_parser(subparser):
+    """Parser is only constructed so that this prints a nice help
+       message with -h. """
+    subparser.add_argument(
+        'name', help='Name of collection to remove')
+
+
+def remove(parser, args):
+    pymod.mc.remove(args.name)
