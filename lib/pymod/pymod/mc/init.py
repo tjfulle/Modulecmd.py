257f2d08bd3d6df2df467adf1518eb84050c32ab
Tim Fuller <tjfulle@sandia.gov>
2019-05-28 08:29:36 -0600

    bug fix

--- lib/pymod/pymod/mc/init.py
+++ lib/pymod/pymod/mc/init.py
@@ -7,6 +7,6 @@ def init(dirnames):
     for dirname in dirnames:
         pymod.mc.use(dirname, append=True)
     if pymod.collection.is_collection(pymod.names.default_user_collection): # pragma: no cover
-        pymod.mc.restore(pymod.names.default_user_collection)
+        pymod.mc.collection.restore(pymod.names.default_user_collection)
     pymod.environ.set_dict(pymod.names.initial_env, initial_env)
     return
