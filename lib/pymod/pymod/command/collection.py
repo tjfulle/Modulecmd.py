3558177ea83628d6268c0789a43b5cac5aa136fb
Tim Fuller <tjfulle@sandia.gov>
2019-06-06 20:49:21 -0600

    collection subcommand

---
+++ lib/pymod/pymod/command/collection.py
@@ -0,0 +1,70 @@
+import sys
+import pymod.mc
+import pymod.collection
+
+description = 'Manipulate collections of modules'
+level = 'short'
+section = 'collections'
+
+
+_subcommands = {}
+
+
+def add_avail_command(parser):
+    def avail(args):
+        s = pymod.collection.avail(terse=args.terse, regex=args.regex)
+        sys.stderr.write(s)
+    p = parser.add_parser('avail', help='List available (saved) collections')
+    p.add_argument(
+        'regex', nargs='?', metavar='regex',
+        help='Highlight available modules matching "regex"')
+    p.add_argument(
+        '-t', '--terse', action='store_true', default=False,
+        help='Display output in terse format [default: %(default)s]')
+    _subcommands['avail'] = avail
+
+
+def add_save_command(parser):
+    def save(args):
+        return pymod.mc.collection.save(args.name)
+    p = parser.add_parser('save', help='Save the current environment')
+    p.add_argument(
+        'name', nargs='?',
+        default=pymod.names.default_user_collection,
+        help='Name of collection to save')
+    _subcommands['save'] = save
+
+
+def add_remove_command(parser):
+    def remove(args):
+        return pymod.mc.collection.remove(args.name)
+    p = parser.add_parser('remove', help='Remove collection')
+    p.add_argument(
+        'name', help='Name of collection to remove')
+    _subcommands['remove'] = remove
+
+
+def add_restore_command(parser):
+    def restore(args):
+        pymod.mc.collection.restore(args.name)
+        pymod.mc.dump()
+    p = parser.add_parser('restore', help='Restore collection')
+    p.add_argument(
+        'name', nargs='?',
+        default=pymod.names.default_user_collection,
+        help='Name of collection to restore')
+    _subcommands['restore'] = restore
+
+
+def setup_parser(subparser):
+    """Parser is only constructed so that this prints a nice help
+       message with -h. """
+    sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='subcommand')
+    add_avail_command(sp)
+    add_save_command(sp)
+    add_remove_command(sp)
+    add_restore_command(sp)
+
+
+def collection(parser, args):
+    _subcommands[args.subcommand](args)
---
+++ lib/pymod/pymod/test/command/collection.py
@@ -0,0 +1,86 @@
+import os
+import pytest
+
+import pymod.mc
+import pymod.paths
+import pymod.environ
+import pymod.collection
+from pymod.main import PymodCommand
+from pymod.error import CollectionNotFoundError
+
+
+@pytest.fixture()
+def modules_path(tmpdir, namespace, modulecmds):
+    m = modulecmds
+    one = tmpdir.mkdir('1')
+    one.join('a.py').write(m.setenv('a'))
+    one.join('b.py').write(m.setenv('b'))
+    one.join('b2.py').write(m.setenv('b2'))
+    two = tmpdir.mkdir('2')
+    two.join('c.py').write(m.setenv('c'))
+    two.join('d.py').write(m.setenv('d'))
+
+    ns = namespace()
+    ns.path = [one.strpath, two.strpath]
+    return ns
+
+def test_command_collection_basic(modules_path, mock_modulepath):
+
+    mock_modulepath(modules_path.path)
+    collection = PymodCommand('collection')
+    a = pymod.mc.load('a')
+    b = pymod.mc.load('b')
+    c = pymod.mc.load('c')
+    d = pymod.mc.load('d')
+
+    collection('save', 'foo')
+
+    pymod.mc.purge()
+    assert not a.is_loaded
+    assert not b.is_loaded
+    assert not c.is_loaded
+    assert not d.is_loaded
+
+    collection('restore', 'foo')
+    for x in 'abcd':
+        m = pymod.modulepath.get(x)
+        assert m.is_loaded
+
+    pymod.mc.purge()
+
+    collection('remove', 'foo')
+    assert pymod.collection.get('foo') is None
+
+
+def test_command_collection_restore_bad(modules_path, mock_modulepath):
+    collection = PymodCommand('collection')
+    with pytest.raises(CollectionNotFoundError):
+        collection('restore', 'fake')
+
+
+def test_command_collection_basic2(modules_path, mock_modulepath):
+
+    mock_modulepath(modules_path.path)
+    collection = PymodCommand('collection')
+    a = pymod.mc.load('a')
+    b = pymod.mc.load('b')
+    c = pymod.mc.load('c')
+    d = pymod.mc.load('d')
+
+    collection('save', 'foo')
+
+    pymod.mc.purge()
+    assert not a.is_loaded
+    assert not b.is_loaded
+    assert not c.is_loaded
+    assert not d.is_loaded
+
+    collection('restore', 'foo')
+    for x in 'abcd':
+        m = pymod.modulepath.get(x)
+        assert m.is_loaded
+
+    collection('avail')
+    collection('avail', '-t')
+    collection('avail', '--terse')
+    collection('avail', 'foo')
