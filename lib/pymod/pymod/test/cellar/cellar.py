7228d62f0cbd39853fd9fa9ccec89fe43f2a4eb3
Tim Fuller <tjfulle@sandia.gov>
2019-07-09 10:40:47 -0600

    new cellar tests

--- lib/pymod/pymod/test/cellar/cellar.py
+++ lib/pymod/pymod/test/cellar/cellar.py
@@ -1,10 +1,28 @@
+import random
+import string
+
 import pymod.mc
 import pymod.names
 import pymod.environ
 import pymod.modulepath
 
 
-def test_mc_cellar(tmpdir, mock_modulepath):
+class dict_with_set(dict):
+    def set(self, key, value):
+        self[key] = value
+
+
+def test_cellar_put_acquire():
+    # Need to write our own
+    env = dict_with_set()
+    random_string = lambda: ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(200))
+    l_orig = [random_string() for i in range(5)]
+    pymod.cellar.put_in_env(env, l_orig)
+    l = pymod.cellar.acquire_from_env(env)
+    assert l == l_orig
+
+
+def test_cellar_environ(tmpdir, mock_modulepath):
     assert pymod.mc._mc._loaded_modules is None
 
     # Need to write our own
@@ -22,3 +40,4 @@ def test_mc_cellar(tmpdir, mock_modulepath):
     loaded_modules = pymod.mc.get_loaded_modules()
     loaded_module_names = ''.join(m.name for m in loaded_modules)
     assert loaded_module_names == names
+
